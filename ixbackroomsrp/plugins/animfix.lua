e0b915f8fb247d287e77e8d1bd7fa46e00cd9007
nic <nicholasbecerra55@gmail.com>
2025-09-27 14:50:43 -0400

    add support for future model anims
    also optimize

--- ixbackroomsrp/plugins/animfix.lua
+++ ixbackroomsrp/plugins/animfix.lua
@@ -2,19 +2,35 @@ PLUGIN.name = "Animation Fix"
 PLUGIN.author = "nicwtf"
 PLUGIN.description = "Fixes animations for factions"
 
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/female_01.mdl", "player")
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/female_02.mdl", "player")
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/female_03.mdl", "player")
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/female_04.mdl", "player")
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/female_06.mdl", "player")
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/female_07.mdl", "player")
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male01.mdl", "player")
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male02.mdl", "player")
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male03.mdl", "player")
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male04.mdl", "player")
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male05.mdl", "player")
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male06.mdl", "player")
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male07.mdl", "player")
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male08.mdl", "player")
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male09.mdl", "player")
-ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male10.mdl", "player")
+local modelClasses = {
+    ["player"] = {
+        "models/willardnetworks_custom/citizens/female_01.mdl",
+        "models/willardnetworks_custom/citizens/female_02.mdl",
+        "models/willardnetworks_custom/citizens/female_03.mdl",
+        "models/willardnetworks_custom/citizens/female_04.mdl",
+        "models/willardnetworks_custom/citizens/female_06.mdl",
+        "models/willardnetworks_custom/citizens/female_07.mdl",
+        "models/willardnetworks_custom/citizens/male01.mdl",
+        "models/willardnetworks_custom/citizens/male02.mdl",
+        "models/willardnetworks_custom/citizens/male03.mdl",
+        "models/willardnetworks_custom/citizens/male04.mdl",
+        "models/willardnetworks_custom/citizens/male05.mdl",
+        "models/willardnetworks_custom/citizens/male06.mdl",
+        "models/willardnetworks_custom/citizens/male07.mdl",
+        "models/willardnetworks_custom/citizens/male08.mdl",
+        "models/willardnetworks_custom/citizens/male09.mdl",
+        "models/willardnetworks_custom/citizens/male10.mdl"
+    },
+
+    --[[
+    ["metrocop"] = {
+        "models/police.mdl"
+    }
+    ]]
+}
+
+for class, models in pairs(modelClasses) do
+    for _, model in ipairs(models) do
+        ix.anim.SetModelClass(model, class)
+    end
+end
