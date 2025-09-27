bc9ae8b54f849e53dc543153bb92ed564faebb62
nic <nicholasbecerra55@gmail.com>
2025-09-27 14:00:37 -0400

    update survivor models
    why the FUCK did i format it like this

--- ixbackroomsrp/schema/factions/sh_citizen.lua
+++ ixbackroomsrp/schema/factions/sh_citizen.lua
@@ -1,37 +1,39 @@
 FACTION.name = "Survivor"
-
 FACTION.description = "You noclipped out of reality and into the Backrooms, a desolate, yellow labyrinth of endless hums and fluorescent lights. Your only goal is to survive the entities that hunt you, as your old life is now a fading memory."
-
 FACTION.color = Color(150, 125, 100, 255)
-
 FACTION.isDefault = true
 
-
+FACTION.models = {
+	"models/willardnetworks/citizens/female_01.mdl",
+	"models/willardnetworks/citizens/female_02.mdl",
+	"models/willardnetworks/citizens/female_03.mdl",
+	"models/willardnetworks/citizens/female_04.mdl",
+	"models/willardnetworks/citizens/female_06.mdl",
+	"models/willardnetworks/citizens/female_07.mdl",
+	
+	"models/willardnetworks/citizens/male01.mdl",
+	"models/willardnetworks/citizens/male02.mdl",
+	"models/willardnetworks/citizens/male03.mdl",
+	"models/willardnetworks/citizens/male04.mdl",
+	"models/willardnetworks/citizens/male05.mdl",
+	"models/willardnetworks/citizens/male06.mdl",
+	"models/willardnetworks/citizens/male07.mdl",
+	"models/willardnetworks/citizens/male08.mdl",
+	"models/willardnetworks/citizens/male09.mdl",
+	"models/willardnetworks/citizens/male10.mdl",
+}
 
 function FACTION:OnCharacterCreated(client, character)
+	local id = Schema:ZeroNumber(math.random(1, 99999), 5)
+	local inventory = character:GetInventory()
 
-local id = Schema:ZeroNumber(math.random(1, 99999), 5)
+	character:SetData("cid", id)
 
-local inventory = character:GetInventory()
-
-
-
-character:SetData("cid", id)
-
-
-
-inventory:Add("camera", 1)
-
-inventory:Add("cid", 1, {
-
-name = character:GetName(),
-
-id = id
-
-})
-
+	inventory:Add("camera", 1)
+	inventory:Add("cid", 1, {
+		name = character:GetName(),
+		id = id
+	})
 end
-
-
 
 FACTION_CITIZEN = FACTION.index
