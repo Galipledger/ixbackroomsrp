a81c1f0ee202aacbc3291c0feee11b062cab0f78
nic <nicholasbecerra55@gmail.com>
2025-09-27 14:22:35 -0400

    fix survivor anims and create plugin

---
+++ ixbackroomsrp/plugins/animfix.lua
@@ -0,0 +1,20 @@
+PLUGIN.name = "Animation Fix"
+PLUGIN.author = "nicwtf"
+PLUGIN.description = "Fixes animations for factions"
+
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/female_01.mdl", "player")
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/female_02.mdl", "player")
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/female_03.mdl", "player")
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/female_04.mdl", "player")
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/female_06.mdl", "player")
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/female_07.mdl", "player")
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male01.mdl", "player")
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male02.mdl", "player")
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male03.mdl", "player")
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male04.mdl", "player")
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male05.mdl", "player")
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male06.mdl", "player")
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male07.mdl", "player")
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male08.mdl", "player")
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male09.mdl", "player")
+ix.anim.SetModelClass("models/willardnetworks_custom/citizens/male10.mdl", "player")
